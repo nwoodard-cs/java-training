0a0edfa478265d83867eeb1839b585379681f1e8
Nicole Woodard <woodard@adobe.com>
2019-08-27 10:20:58 -0600

    Add car class

---
+++ src/Car.java
@@ -0,0 +1,23 @@
+import java.awt.*;
+
+public class Car {
+  double avgMPG;
+  String licensePlate;
+  Color paintColor;
+  boolean tailLightsWork;
+
+  public Car(double _avgMPG, 
+             String _licensePlate, 
+             Color _paintColor, 
+             boolean _tailLightsWork) {
+    this.avgMPG = _avgMPG;
+    this.licensePlate = _licensePlate;
+    this.paintColor = _paintColor;
+    this.tailLightsWork = _tailLightsWork;
+  }
+
+  public void setPaintColor(Color newPaintColor) {
+    this.paintColor = newPaintColor;
+  }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.awt.*;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello nicole");
+    Car fordTaurus = new Car(15, "48WI12", Color.ORANGE, false);
+    Car hondaFit = new Car(40, "C984EK", Color.LIGHT_GRAY, true);
+    hondaFit.setPaintColor(Color.GREEN);
+    System.out.println(hondaFit.paintColor.toString() + " " + fordTaurus.paintColor.toString());
   }
 }
