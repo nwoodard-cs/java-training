f5d0e10ba99bb1fd8f309b95b5fe7f58da010842
Nicole Woodard <woodard@adobe.com>
2019-08-27 10:31:52 -0600

    Add scanner for user input

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-    String userInput = "reading";
+
+    Scanner sc = new Scanner(System.in);
+    System.out.println("What's your user number?");
+    int userNumber = sc.nextInt();
+    System.out.println("Enter a word:");
+    String userInput = sc.next();
     String upperUserInput = userInput.toUpperCase();
     char firstUserChar = userInput.charAt(0);
-    boolean hasDing = userInput.contains("ding");
-    System.out.println(upperUserInput + " " + firstUserChar + "Has ding? " + hasDing);
+    System.out.println("Enter a search string: ");
+    String searchString = sc.next();
+    boolean hasDing = userInput.contains("ding".toLowerCase());
+    System.out.println("User #" + userNumber + " " + 
+                        upperUserInput + " " + firstUserChar + ". Has " + 
+                        searchString + "? "+ hasDing);
+    sc.close();
 
   }
 }
